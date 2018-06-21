ebfc875ba79fcdf9b90482b1cfed43d39d405daa
GameDev12-Keith <36052753+GameDev12-Keith@users.noreply.github.com>
2018-06-21 11:14:13 -0700

    Add files via upload

--- CON3/offline.js
+++ CON3/offline.js
@@ -1,5 +1,5 @@
 ﻿{
-	"version": 1528822577,
+	"version": 1529604743,
 	"fileList": [
 		"data.js",
 		"c2runtime.js",
@@ -17,6 +17,8 @@
 		"images/sprite-sheet0.png",
 		"images/sprite2-sheet0.png",
 		"images/doubleturret-sheet0.png",
+		"images/enemy2-sheet0.png",
+		"images/enemy3-sheet0.png",
 		"icon-16.png",
 		"icon-32.png",
 		"icon-114.png",
